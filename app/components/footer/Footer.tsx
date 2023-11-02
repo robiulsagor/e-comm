362083a53b839883d5879026582f11479c44c711
robiulsagor <robiul100.me@gmail.com>
2023-11-02 18:49:09 +0600

    initial commit

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link"
+import { FaFacebookF } from "react-icons/fa"
+import { BiLogoFacebookCircle } from "react-icons/bi"
+import { FaXTwitter } from "react-icons/fa6"
+import { AiOutlineInstagram, AiFillInstagram } from "react-icons/ai"
+import { AiOutlineYoutube, AiFillYoutube } from "react-icons/ai"
+import Container from "../Container"
+
+const Footer = () => {
+    return (
+        <div className="bg-slate-800 text-white pt-10 pb-6 xl:pt-20 xl:pb-14  mt-6">
+            <Container>
+                <div className="grid gap-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                    <div>
+                        <h2 className="mb-4">Shop Categories</h2>
+                        <ul className="flex flex-col text-[13px] md:text-[15px] gap-1 text-slate-400 ">
+
+                            <Link href={"/"}>Phones</Link>
+                            <Link href={"/"}>Laptops</Link>
+                            <Link href={"/"}>Desktops</Link>
+                            <Link href={"/"}>Watches</Link>
+                            <Link href={"/"}>TVs</Link>
+                            <Link href={"/"}>Accessories</Link>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2 className="mb-4">Customer Sevices</h2>
+                        <ul className="flex flex-col text-[13px] md:text-[15px] gap-1 text-slate-400 ">
+
+                            <Link href={"/"}>Contact Us</Link>
+                            <Link href={"/"}>Shipping Policy</Link>
+                            <Link href={"/"}>Returns &amp; Exchanges</Link>
+                            <Link href={"/"}>Watches</Link>
+                            <Link href={"/"}>FAQs</Link>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2 className="mb-4">Follow Us</h2>
+                        <ul className="flex flex-row text-[13px] md:text-[15px] gap-2 text-slate-300 ">
+
+                            <Link href={"/"}>
+                                <BiLogoFacebookCircle size={20} />
+                            </Link>
+                            <Link href={"/"}>
+                                <FaXTwitter size={20} />
+                            </Link>
+                            <Link href={"/"}>
+                                <AiFillInstagram size={20} />
+                            </Link>
+                            <Link href={"/"}>
+                                <AiFillYoutube size={20} />
+                            </Link>
+
+                        </ul>
+                    </div>
+
+                    <div className="col-span-2">
+                        <h2 className="mb-4">About Us</h2>
+                        <ul className="flex flex-col text-[13px] md:text-[15px] gap-1  text-slate-400 text-justify">
+
+                            <p className="text-slate-400 text-justify">
+                                At our electronics store, we are dedicated to provide the latest and greatest devices and accessories to our customers. With a wide selection of phones, tvs, laptops, desktops etc.
+                            </p>
+
+                            <p className="mt-2">&copy; {new Date().getFullYear()} E-Comm. All rights reserved.</p>
+                        </ul>
+                    </div>
+
+
+
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Footer
---
+++ app/components/nav/Navbar.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import Link from "next/link"
+import { Redressed } from "next/font/google"
+import { BsCart3 } from "react-icons/bs"
+import { AiOutlineUser } from "react-icons/ai"
+import { BiSolidChevronDownSquare } from "react-icons/bi"
+import Container from "../Container"
+import { useState } from "react"
+
+const redressed = Redressed({ subsets: ["latin"], weight: "400" })
+
+const Navbar = () => {
+    const [userOptions, setUserOptions] = useState(false)
+
+    return (
+        <div className="sticky top-0 left-0 w-full bg-slate-200">
+            <div className="">
+                <Container>
+                    <div className="flex items-center justify-between p-4">
+                        <Link href={"/"}
+                            className={`${redressed.className} text-2xl`}>E-Comm</Link>
+
+                        <div className="hidden md:block border md:w-[350px] lg:w-[380px] xl:w-[420px] text-sm lg:text-[16px]">
+                            <div className="border border-slate-500 rounded-lg overflow-hidden flex ">
+                                <input type="text" name="" id="" className="p-2 outline-none flex-grow " />
+                                <button className=" bg-black text-slate-100 py-2 px-3 hover:bg-slate-700 transition-all  ">Submit</button>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between items-center gap-8 lg:gap-12 relative  select-none">
+                            <div className="cursor-pointer relative">
+                                <BsCart3 className="w-[22px] h-[22px] md:w-[27px] md:h-[27px]" />
+                                <span className="absolute -top-4 -right-4 bg-slate-900 text-slate-100  rounded-full text-sm px-2 py-1">2</span>
+                            </div>
+
+                            <div className="border border-slate-500 flex gap-2 items-end rounded-3xl p-2 cursor-pointer z-30"
+                                onClick={() => setUserOptions(prev => !prev)}>
+                                <AiOutlineUser className="w-[22px] h-[22px] md:w-[27px] md:h-[27px]" />
+                                <BiSolidChevronDownSquare className="w-[18px] h-[18px] md:w-[21px] md:h-[21px]" />
+                            </div>
+
+                            <div className={` top-full right-0  bg-slate-50 absolute z-30 overflow-hidden transition-all duration-300 rounded-lg shadow-xl ${userOptions ? "w-auto" : "w-0"}`}>
+                                <ul className="flex flex-col w-40">
+                                    <Link href={"/"} className="py-2 px-4 hover:bg-slate-300">Login</Link>
+                                    <Link href={"/"} className="py-2 px-4 hover:bg-slate-300">Register</Link>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+
+            {/* User options overlay containers */}
+            <div className={` bg-[#fdfcfc7f]   w-screen h-screen left-0 top-0 z-10 ${userOptions ? "fixed z-10" : "hidden"}`} onClick={() => setUserOptions(false)} />
+        </div>
+    )
+}
+
+export default Navbar
